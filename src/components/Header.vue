e2043af149cf83544223b445ba909fe8012d0fdb
c4r7u <nicolo.carluccio3@gmail.com>
2022-01-11 17:13:41 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div class="nav">
+            <input type="text" placeholder="Cerca">
+
+            <button>Cerca</button>
+        </div>
+    </header>
+</template>
+
+<script>
+
+</script>
+
+
+<style lang="scss">
+.nav {
+    display: flex;
+}
+</style>
